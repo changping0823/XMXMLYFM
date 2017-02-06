cbf3a636701f9aa6363266c6138a410f8d2816c8
changping0823 <RCP@renchangpingdeMacBook-Pro.local>
2017-02-06 14:52:32 +0800

    添加代码

--- XMXMLYFM/Classes/Main(主要)/Mine(我的)/ViewControllers/MineViewController.swift
+++ XMXMLYFM/Classes/Main(主要)/Mine(我的)/ViewControllers/MineViewController.swift
@@ -140,7 +140,11 @@ extension MineViewController{
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let contentOffsety = scrollView.contentOffset.y
+        
         print(contentOffsety)
+        print(contentOffsety)
+        print(contentOffsety)
+        
         /// 是否显示 shadowImage(分割线)
         self.navigationController?.navigationBar.showShadow = (contentOffsety > bgHeight - XMNavHeight) ? true : false
         
